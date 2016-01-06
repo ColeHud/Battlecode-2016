faf40177bc8add56c8c00daf969c1752adb96511
ColeHud <cmhudson11@gmail.com>
2016-01-05 22:15:28 -0500

    Added a test map

--- scaffold/src/TestPlayer/RobotPlayer.java
+++ scaffold/src/TestPlayer/RobotPlayer.java
@@ -1,15 +1,90 @@
 package TestPlayer;
 
 import battlecode.common.*;
+
 import java.util.Random;
 
 public class RobotPlayer 
 {
-	public static void run(RobotController rc)
+	public static Direction[] directions = Direction.values();
+	public static Random randall;
+
+	public static void run(RobotController rc) throws GameActionException
 	{
-		while(true)
+		randall = new Random(rc.getID());
+
+		if(rc.getType() == RobotType.ARCHON)
 		{
-			Clock.yield();
+			while(true)
+			{
+				Direction directionToMove = directions[randall.nextInt(directions.length)];
+
+				if(rc.canBuild(directionToMove, RobotType.SOLDIER) && rc.isCoreReady())
+				{
+					rc.build(directionToMove, RobotType.SOLDIER);
+				}
+
+				if(rc.canMove(directionToMove) && rc.isCoreReady())
+				{
+					rc.move(directionToMove);
+				}
+
+				Clock.yield();
+			}
+		}
+		else if(rc.getType() == RobotType.SOLDIER)
+		{
+			while(true)
+			{
+				/*
+				RobotInfo[] enemies = rc.senseNearbyRobots(RobotType.SOLDIER.attackRadiusSquared, Team.ZOMBIE);
+				if(enemies.length > 0)
+				{
+					RobotInfo enemyToAttack = enemies[0];
+					MapLocation loc = enemyToAttack.location;
+
+					if(rc.canAttackLocation(loc) && rc.isCoreReady() && rc.isWeaponReady())
+					{
+						rc.attackLocation(loc);
+					}
+				}
+				*/
+				
+				if(rc.getHealth() < RobotType.SOLDIER.maxHealth)
+				{
+					rc.disintegrate();
+				}
+				
+				
+				Direction directionToMove = directions[randall.nextInt(directions.length)];
+				if(rc.canMove(directionToMove) && rc.isCoreReady())
+				{
+					rc.move(directionToMove);
+				}
+
+				Clock.yield();
+			}
+		}
+		else if(rc.getType() == RobotType.SCOUT)
+		{
+			while(true)
+			{
+				Clock.yield();
+			}
+		}
+		else if(rc.getType() == RobotType.TURRET)
+		{
+			while(true)
+			{
+				Clock.yield();
+			}
+		}
+		else if(rc.getType() == RobotType.VIPER)
+		{
+			while(true)
+			{
+				Clock.yield();
+			}
 		}
 	}
 }
