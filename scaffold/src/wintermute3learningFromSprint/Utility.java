a1e247ee2303ef9b10dc7d55f2ddf30e41f47aa1
ColeHud <cmhudson11@gmail.com>
2016-01-14 12:26:56 -0500

    added a bot from learning from the sprint tournament

---
+++ scaffold/src/wintermute3learningFromSprint/Utility.java
@@ -0,0 +1,11 @@
+package wintermute3learningFromSprint;
+import battlecode.common.*;
+
+public class Utility 
+{
+	//constants 
+	public static final int MINIMUM_TIME_TO_TURTLE = 100;
+	
+	//codes
+	public static final int STRATEGY_CODE = 77525;
+}
