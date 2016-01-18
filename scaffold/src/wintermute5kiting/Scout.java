8dfc4caaab3e003670d99ec621919653b0868ded
ColeHud <cmhudson11@gmail.com>
2016-01-17 20:52:26 -0500

    created wintermute5 kiting

---
+++ scaffold/src/wintermute5kiting/Scout.java
@@ -0,0 +1,125 @@
+package wintermute5kiting;
+import battlecode.common.*;
+
+import java.util.*;
+
+public class Scout 
+{
+	public static RobotController rc;
+	public static Random rand;
+	
+	//for pathing
+	public static ArrayList<MapLocation> slugTrail = new ArrayList<MapLocation>();
+	
+	public static MapLocation[] enemyArchonsInitialPosition;
+	
+	//scouting
+	public static MapLocation pivotLocation = null;
+	public static Direction directionToMove = null;
+	
+	//broadcasting
+	public static int broadcastsThisTurn = 0;
+	public static MapLocation spawnLocation;
+
+	public static void run() throws GameActionException
+	{
+		rc = RobotPlayer.rc;
+		rand = new Random(rc.getID());
+		
+		enemyArchonsInitialPosition = rc.getInitialArchonLocations(rc.getTeam().opponent());
+		
+		pivotLocation = rc.getLocation();
+		spawnLocation = pivotLocation;
+		while(true)
+		{
+			broadcastsThisTurn = 0;
+			//signal
+			RobotInfo[] nearbyFoes = rc.senseHostileRobots(rc.getLocation(), RobotType.SCOUT.sensorRadiusSquared);
+			if(nearbyFoes.length > 5)
+			{
+				broadcast(Utility.CLUMP_OF_FOES_CODE, Utility.CLUMP_OF_FOES_CODE);
+			}
+			int numberOfEnemyTurrets = 0;
+			for(RobotInfo foe : nearbyFoes)
+			{
+				if(foe.type == RobotType.TURRET || foe.type == RobotType.TTM)
+				{
+					numberOfEnemyTurrets++;
+				}
+				if(foe.type == RobotType.ZOMBIEDEN)
+				{
+					broadcast(Utility.ZOMBIE_DEN, Utility.ZOMBIE_DEN);
+				}
+			}
+			if(numberOfEnemyTurrets > 3)
+			{
+				broadcast(Utility.ENEMY_TURTLE, Utility.ENEMY_TURTLE);
+			}
+			
+			//initial case
+			if(directionToMove == null)
+			{
+				//choose a random direction to move in
+				Direction[] possibleDirections = Direction.values();
+				directionToMove = possibleDirections[rand.nextInt(possibleDirections.length)];
+			}
+			
+			//every other case
+			if(rc.canMove(directionToMove))
+			{
+				//if there are foes nearby, choose another direction
+				/*
+				RobotInfo[] foes = rc.senseHostileRobots(rc.getLocation(), RobotType.SCOUT.sensorRadiusSquared);
+				if(foes.length > 0)
+				{
+					//set the direction to move to the enemy so you move away from it
+					directionToMove = rc.getLocation().directionTo(foes[0].location);
+					getNewDirection();
+					if(rc.canMove(directionToMove) && rc.isCoreReady())
+					{
+						rc.move(directionToMove);
+					}
+				}
+				*/
+				/*else */if(rc.isCoreReady())//if there aren't enemies nearby, move int the current direction
+				{
+					rc.move(directionToMove);
+				}
+			}
+			else
+			{
+				//if you can't move in the current direction, find a new one
+				getNewDirection();
+			}
+
+			Clock.yield();
+		}
+	}	
+	
+	//broadcast
+	public static void broadcast(int message1, int message2) throws GameActionException
+	{
+		if(broadcastsThisTurn < 20)
+		{
+			//calculate the radius to broadcast
+			int radius = rc.getLocation().distanceSquaredTo(spawnLocation);
+			
+			rc.broadcastMessageSignal(message1, message2, radius);
+		}
+	}
+	
+	//get a new direction to move
+	public static void getNewDirection()
+	{
+		pivotLocation = rc.getLocation();
+		
+		//get a new direction to go
+		ArrayList<Direction> possibleDirections = Utility.arrayListOfDirections();
+		possibleDirections.remove(directionToMove);
+		possibleDirections.remove(directionToMove.rotateRight());
+		possibleDirections.remove(directionToMove.rotateLeft());
+		possibleDirections.remove(directionToMove.opposite());
+		
+		directionToMove = possibleDirections.get(rand.nextInt(possibleDirections.size()));
+	}
+}
---
+++ scaffold/src/wintermute5kiting/Turret.java
@@ -0,0 +1,199 @@
+package wintermute5kiting;
+import java.util.*;
+
+import battlecode.common.*;
+
+public class Turret 
+{
+	public static RobotController rc;
+
+	//slug trail for pathing
+	public static ArrayList<MapLocation> slugTrail = new ArrayList<MapLocation>(20);
+	
+	//goal maplocation
+	public static MapLocation goal = null;
+	
+	public static Random rand;
+	public static Team myTeam;
+
+	public static void run() throws GameActionException
+	{
+		rc = RobotPlayer.rc;
+		rand = new Random(rc.getID());
+		myTeam = rc.getTeam();
+		while(true)
+		{	
+			//read signals
+			Signal[] signals = rc.emptySignalQueue();
+			for(Signal signal : signals)
+			{
+				int[] message = signal.getMessage();
+				if(message != null)
+				{
+					if(message[0] == Utility.ENEMY_TURTLE)
+					{
+						goal = signal.getLocation();
+					}
+				}
+			}
+			
+			//sense the robots around you
+			RobotInfo[] nearbyBots = rc.senseNearbyRobots();
+			RobotInfo[] nearbyFoes = rc.senseHostileRobots(rc.getLocation(), RobotType.TURRET.sensorRadiusSquared);
+			ArrayList<RobotInfo> nearbyFriendlySoldiers = new ArrayList<RobotInfo>();
+			for(RobotInfo robot : nearbyBots)
+			{
+				if(robot.type == RobotType.SOLDIER && robot.team == myTeam)
+				{
+					nearbyFriendlySoldiers.add(robot);
+				}
+			}
+			
+			//ATTACK
+			if(nearbyFoes.length > 0)
+			{
+				//1. Big zombies
+				//2. the foe with lowest health
+				RobotInfo foeToAttack = null;
+				double lowestHealth = 100000;
+				for(RobotInfo foe : nearbyFoes)
+				{
+					if(foe.type == RobotType.BIGZOMBIE)
+					{
+						foeToAttack = foe;
+						break;
+					}
+					else if(foe.health < lowestHealth)
+					{
+						lowestHealth = foe.health;
+						foeToAttack = foe;
+					}
+				}
+				
+				if(foeToAttack != null)
+				{
+					if(rc.isCoreReady() && rc.isWeaponReady() && rc.canAttackLocation(foeToAttack.location))
+					{
+						rc.attackLocation(foeToAttack.location);
+					}
+				}
+			}
+			
+			//get the average nearby soldier location
+			int numberOfNearbyFriendlySoldiers = nearbyFriendlySoldiers.size();
+			if(numberOfNearbyFriendlySoldiers > 0)
+			{
+				int averagex = 0;
+				int averagey = 0;
+				for(RobotInfo soldier : nearbyFriendlySoldiers)
+				{
+					averagex += soldier.location.x;
+					averagey += soldier.location.y;
+				}
+				averagex /= numberOfNearbyFriendlySoldiers;
+				averagey /= numberOfNearbyFriendlySoldiers;
+				
+				goal = new MapLocation(averagex, averagey);
+			}
+			
+			if(nearbyFoes.length == 0 && goal != null)
+			{
+				moveToLocation(goal);
+			}
+			else
+			{
+				if(rc.getType() == RobotType.TTM)
+				{
+					rc.unpack();
+				}
+			}
+			
+			Clock.yield();
+		}
+	}
+	
+	//move to open adjacent location
+	public static void moveToOpenAdjacentLocation() throws GameActionException
+	{
+		ArrayList<Direction> directions = Utility.arrayListOfDirections();
+		while(directions.size() > 0)
+		{
+			int index = rand.nextInt(directions.size());
+			if(rc.canMove(directions.get(index)) && rc.isCoreReady())
+			{
+				rc.move(directions.get(index));
+			}
+			else
+			{
+				directions.remove(index);
+			}
+		}
+	}
+
+	//move to a location
+	public static void moveToLocation(MapLocation location) throws GameActionException
+	{	
+		if(rc.isCoreReady())
+		{
+			if(rc.getType() == RobotType.TURRET)
+			{
+				rc.pack();
+			}
+
+			MapLocation currentLocation = rc.getLocation();
+
+			//trim the slug trail to size 20
+			slugTrail.add(currentLocation);
+			if(slugTrail.size() > 20)
+			{
+				slugTrail.remove(0);
+			}
+
+			Direction candidateDirection = currentLocation.directionTo(location);
+			MapLocation locationInDirection = currentLocation.add(candidateDirection);
+			double rubbleAtLocation = rc.senseRubble(locationInDirection);
+
+			if(slugTrail.contains(locationInDirection))//if you've just been there
+			{
+				for(int i = 0; i < 8; i++)
+				{
+					candidateDirection = candidateDirection.rotateRight();
+					locationInDirection = currentLocation.add(candidateDirection);
+					rubbleAtLocation = rc.senseRubble(locationInDirection);
+
+					if(rc.isCoreReady() && rc.canMove(candidateDirection) && slugTrail.contains(locationInDirection) == false && rubbleAtLocation < GameConstants.RUBBLE_OBSTRUCTION_THRESH)//move there then return
+					{
+						rc.setIndicatorString(0, "Trying to move");
+						rc.move(candidateDirection);
+						return;
+					}
+
+				}
+			}
+			else
+			{
+				if(rc.isCoreReady() && rc.canMove(candidateDirection) && rubbleAtLocation < GameConstants.RUBBLE_OBSTRUCTION_THRESH)
+				{
+					rc.move(candidateDirection);
+				}
+				else
+				{
+					for(int i = 0; i < 8; i++)
+					{
+						candidateDirection = candidateDirection.rotateRight();
+						locationInDirection = currentLocation.add(candidateDirection);
+						rubbleAtLocation = rc.senseRubble(locationInDirection);
+
+						if(rc.isCoreReady() && rc.canMove(candidateDirection) && slugTrail.contains(locationInDirection) == false && rubbleAtLocation < GameConstants.RUBBLE_OBSTRUCTION_THRESH)//move there then return
+						{
+							rc.setIndicatorString(0, "Trying to move");
+							rc.move(candidateDirection);
+							return;
+						}
+
+					}
+				}
+			}
+		}
+	}
+}
